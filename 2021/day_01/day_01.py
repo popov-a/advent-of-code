76ed55c93b5b65c31406faf9a2e237ba2a941dd8
bio-ton <popov_a_v@mail.ru>
2021-12-13 15:24:44 +0400

    day 2 solution

---
+++ 2021/day_01/day_01.py
@@ -0,0 +1,39 @@
+from typing import List
+
+class Day_01:
+    def read_input(self, file_name: str):
+        self.nums = list()
+        with open(file_name) as f:
+            for line in f:
+                self.nums.append(int(line.strip()))
+        return None
+
+    def part1(self) -> int:
+        count = 0
+        for i in range(1, len(self.nums)):
+            if self.nums[i] > self.nums[i - 1]:
+                count += 1
+        return count
+
+    def part2(self) -> int:
+        count = 0
+        sum = 0
+        for i in range(len(self.nums)):
+            prev_sum = sum
+            sum += self.nums[i]
+            if i > 2:
+                sum -= self.nums[i - 3]
+                if sum > prev_sum:
+                    count += 1
+        return count
+
+solution = Day_01()
+
+solution.read_input('test.txt')
+print(solution.part1())
+print(solution.part2())
+
+solution.read_input('input.txt')
+print(solution.part1())
+print(solution.part2())
+
---
+++ 2021/day_02/day_02.py
@@ -0,0 +1,49 @@
+from typing import List
+
+class Day_02:
+    def read_input(self, file_name: str):
+        self.cmds = list()
+        with open(file_name) as f:
+            for line in f:
+                self.cmds.append(line.strip())
+        return None
+
+    def part1(self) -> int:
+        pos_x = 0
+        pos_y = 0
+        for cmd in self.cmds:
+            cmd_name, num = cmd.split()
+            num = int(num)
+            if cmd_name == 'forward':
+                pos_x += num
+            elif cmd_name == 'down':
+                pos_y += num
+            elif cmd_name == 'up':
+                pos_y -= num
+        return pos_x * pos_y
+
+    def part2(self) -> int:
+        pos_x = 0
+        pos_y = 0
+        iam = 0
+        for cmd in self.cmds:
+            cmd_name, num = cmd.split()
+            num = int(num)
+            if cmd_name == 'forward':
+                pos_x += num
+                pos_y += num * iam
+            elif cmd_name == 'down':
+                iam += num
+            elif cmd_name == 'up':
+                iam -= num
+        return pos_x * pos_y
+
+solution = Day_02()
+
+solution.read_input('test.txt')
+print(solution.part1())
+print(solution.part2())
+
+solution.read_input('input.txt')
+print(solution.part1())
+print(solution.part2())
