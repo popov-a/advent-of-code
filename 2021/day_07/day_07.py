3a0d958daec7fdf869462fb2d7714c6d42a17f8b
bio-ton <popov_a_v@mail.ru>
2021-12-27 16:28:32 +0400

    day 7 solution

--- 2021/day_07/day_07.py
+++ 2021/day_07/day_07.py
@@ -2,30 +2,37 @@ from collections import Counter
 
 class Day_07:
     def part1(self, file_name: str) -> int:
-        with open(file_name) as f:
-            nums = list(map(int, f.readline().strip().split(',')))
-        fishs = dict(Counter(nums))
-        for i in range(80):
-            fishs_0 = fishs.get(0, 0)
-            for j in range(0, 9):
-                fishs[j] = fishs.get(j + 1, 0)
-            fishs[8] = fishs_0
-            fishs[6] = fishs.get(6, 0) + fishs_0
-        return sum(fishs.values())
+        nums = [int(x) for x in open(file_name).readline().strip().split(',')]
+        nums.sort()
+        center = nums[len(nums) // 2]
+        cost = 0
+        for item in nums:
+            cost += abs(center - item)
+        return cost
+
 
     def part2(self, file_name: str) -> int:
-        with open(file_name) as f:
-            nums = list(map(int, f.readline().strip().split(',')))
-        fishs = dict(Counter(nums))
-        for i in range(256):
-            fishs_0 = fishs.get(0, 0)
-            for j in range(0, 9):
-                fishs[j] = fishs.get(j + 1, 0)
-            fishs[8] = fishs_0
-            fishs[6] = fishs.get(6, 0) + fishs_0
-        return sum(fishs.values())
+        def sum3(x: int) -> int:
+            return sum(abs(x - item) * (abs(x - item) + 1) // 2 for item in nums)
+
+        nums = [int(x) for x in open(file_name).readline().strip().split(',')]
+        min_sum = 1e100
+        for x in range(min(nums), max(nums) + 1):
+            my_sum = sum3(x)
+            if my_sum < min_sum:
+                min_sum = my_sum
+                print(x)
+        return min_sum
 
 
 solution = Day_07()
-print(solution.part1('input.txt'))
+#print(solution.part1('test.txt'))
+#print(solution.part1('input.txt'))
+print(solution.part2('test.txt'))
 print(solution.part2('input.txt'))
+
+nums = sorted([int(x) for x in open('input.txt').readline().strip().split(',')])
+print(sum(nums) / len(nums))
+x = sum(nums) // len(nums)
+print(sum(abs(x - item) * (abs(x - item) + 1) // 2 for item in nums))
+#print(sorted([int(x) for x in open('test.txt').readline().strip().split(',')]))
